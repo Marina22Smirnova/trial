4f3860c2124af3962a4f464efcc4a908101bcf3e
Marina22Smirnova <108875888+Marina22Smirnova@users.noreply.github.com>
2022-07-11 08:19:02 +0300

    Add files via upload

---
+++ src/Array.java
@@ -0,0 +1,11 @@
+public class Array {
+
+    public static void main(String[] args) {
+        int[] arr = new int[12];
+        for (int i = 0; i < 12; i++) {
+            arr[i] = i + 1;
+            System.out.println(arr[i]);
+            System.out.println("Hello World");
+        }
+    }
+}
